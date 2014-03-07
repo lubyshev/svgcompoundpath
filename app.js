1bab235aff54814360a52450fd3aee081d857043
lubyshev <lubyshev@gmail.com>
2014-03-07 12:39:48 +0400

    Code review fix 001

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
+'use strict';
+
 var fs = require('fs');
 var parseSvg = require('./svgcompoundpath');
 var SvgPath = require('svgpath');
 
 var fileSource = process.argv[2] ? process.argv[2] : null;
 var fileDestination = process.argv[3] ? process.argv[3] : null;
-
-if (!fileSource) {
-  console.log('Syntax:');
-  console.log('$ node ./app.js source.svg [destionation.svg]');
-} else {
-
-  var source = fs.readFileSync(fileSource, {
-    'encoding' : 'utf8'
-  });
-  var pathObj = import_svg_image(source, fileDestination);
-
-}
 
 function import_svg_image(data, file) {
   var parsed = parseSvg(data);
@@ -35,7 +25,8 @@ function import_svg_image(data, file) {
           '<?xml version="1.0" standalone="no"?>',
           '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
           '<svg x="$x" y="$y" width="$width" height="$height" xmlns="http://www.w3.org/2000/svg">',
-          '<path d="$path" />', '</svg>' ].join('\n');
+          '<path d="$path" />', '</svg>'
+        ].join('\n');
 
       [ 'x', 'y', 'width', 'height', 'path' ].forEach(function(val) {
         var pat = new RegExp('\\$' + val, 'g');
@@ -47,10 +38,23 @@ function import_svg_image(data, file) {
           'encoding' : 'utf8'
         });
         console.log('Output: ' + file);
-      } else
+      } else {
         console.log(newDocument);
+      }
       return parsed;
     }
   }
   console.log("There are some errors.");
+}
+
+
+if (!fileSource) {
+  console.log('Syntax:');
+  console.log('$ node ./app.js source.svg [destionation.svg]');
+} else {
+  var source = fs.readFileSync(fileSource, {
+    'encoding' : 'utf8'
+  });
+  //var pathObj =
+  import_svg_image(source, fileDestination);
 }
--- svgcompoundpath.js
+++ svgcompoundpath.js
@@ -1,86 +1,138 @@
-/**
- * Function to convert svg document to the compound path
- * 
- * IN: string SVG document
- * 
- * OUT: object { path, x, y, width, height, missedTags[], missedAttrs[], ok
- * (boolean) - true if svg converted without distortion }
- * 
- */
+'use strict';
 
 var XMLDOMParser = require('xmldom').DOMParser;
 var _ = require('lodash');
 var SvgPath = require('svgpath');
 
-var allowedTags = [ 'a', 'altGlyph', 'altGlyphDef', 'altGlyphItem', 'animate',
-    'animateColor', 'animateMotion', 'animateTransform', 'circle', 'clipPath',
-    'color-profile', 'cursor', 'defs', 'desc', 'ellipse', 'feBlend',
-    'feColorMatrix', 'feComponentTransfer', 'feComposite', 'feConvolveMatrix',
-    'feDiffuseLighting', 'feDisplacementMap', 'feDistantLight', 'feFlood',
-    'feFuncA', 'feFuncB', 'feFuncG', 'feFuncR', 'feGaussianBlur', 'feImage',
-    'feMerge', 'feMergeNode', 'feMorphology', 'feOffset', 'fePointLight',
-    'feSpecularLighting', 'feSpotLight', 'feTile', 'feTurbulence', 'filter',
-    'font', 'font-face', 'font-face-format', 'font-face-name', 'font-face-src',
-    'font-face-uri', 'foreignObject', 'g', 'glyph', 'glyphRef', 'hkern',
-    'image', 'line', 'linearGradient', 'marker', 'mask', 'metadata',
-    'missing-glyph', 'mpath', 'path', 'pattern', 'polygon', 'polyline',
-    'radialGradient', 'rect', 'script', 'set', 'stop', 'style', 'svg',
-    'switch', 'symbol', 'text', 'textPath', 'title', 'tref', 'tspan', 'use',
-    'view', 'vkern', ];
+var allowedTags = {
+  desc: true,
+  title: true
+};
 
-var allowedAttrs = [ 'accent-height', 'accumulate', 'additive', 'alphabetic',
-    'amplitude', 'arabic-form', 'ascent', 'attributeName', 'attributeType',
-    'azimuth', 'baseFrequency', 'baseProfile', 'bbox', 'begin', 'bias', 'by',
-    'calcMode', 'cap-height', 'class', 'clipPathUnits', 'contentScriptType',
-    'contentStyleType', 'cx', 'cy', 'd', 'descent', 'diffuseConstant',
-    'divisor', 'dur', 'dx', 'dy', 'edgeMode', 'elevation', 'end', 'exponent',
-    'externalResourcesRequired', 'fill', 'filterRes', 'filterUnits',
-    'font-family', 'font-size', 'font-stretch', 'font-style', 'font-variant',
-    'font-weight', 'format', 'from', 'fx', 'fy', 'g1', 'g2', 'glyph-name',
-    'glyphRef', 'gradientTransform', 'gradientUnits', 'hanging', 'height',
-    'horiz-adv-x', 'horiz-origin-y', 'id', 'ideographic', 'in', 'in2',
-    'intercept', 'k', 'k1', 'k2', 'k3', 'k4', 'kernelMatrix',
-    'kernelUnitLength', 'keyPoints', 'keySplines', 'keyTimes', 'lang',
-    'lengthAdjust', 'limitingConeAngle', 'local', 'markerHeight',
-    'markerUnits', 'markerWidth', 'maskContentUnits', 'maskUnits',
-    'mathematical', 'max', 'media', 'method', 'min', 'mode', 'name', 'name',
-    'numOctaves', 'offset', 'onabort', 'onactivate', 'onbegin', 'onclick',
-    'onend', 'onerror', 'onfocusin', 'onfocusout', 'onload', 'onmousedown',
-    'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onrepeat',
-    'onresize', 'onscroll', 'onunload', 'onzoom', 'operator', 'order',
-    'orient', 'orientation', 'origin', 'overline-position',
-    'overline-thickness', 'panose-1', 'path', 'pathLength',
-    'patternContentUnits', 'patternTransform', 'patternUnits', 'points',
-    'pointsAtX', 'pointsAtY', 'pointsAtZ', 'preserveAlpha',
-    'preserveAspectRatio', 'primitiveUnits', 'r', 'radius', 'refX', 'refY',
-    'rendering-intent', 'repeatCount', 'repeatDur', 'requiredExtensions',
-    'requiredFeatures', 'restart', 'result', 'rotate', 'rx', 'ry', 'scale',
-    'seed', 'slope', 'spacing', 'specularConstant', 'specularExponent',
-    'spreadMethod', 'startOffset', 'stdDeviation', 'stemh', 'stemv',
-    'stitchTiles', 'strikethrough-position', 'strikethrough-thickness',
-    'string', 'style', 'surfaceScale', 'systemLanguage', 'tableValues',
-    'target', 'targetX', 'targetY', 'textLength', 'title', 'to', 'transform',
-    'type', 'u1', 'u2', 'underline-position', 'underline-thickness', 'unicode',
-    'unicode-range', 'units-per-em', 'v-alphabetic', 'v-hanging',
-    'v-ideographic', 'v-mathematical', 'values', 'values', 'version',
-    'vert-adv-y', 'vert-origin-x', 'vert-origin-y', 'viewBox', 'viewTarget',
-    'width', 'widths', 'x', 'x-height', 'x1', 'x2', 'xChannelSelector',
-    'xlink:actuate', 'xlink:arcrole', 'xlink:href', 'xlink:role', 'xlink:show',
-    'xlink:title', 'xlink:type', 'xml:base', 'xml:lang', 'xml:space', 'y',
-    'y1', 'y2', 'yChannelSelector', 'z', 'zoomAndPan', ];
+var allowedAttrs = _.mapValues(
+  _.zipObject([
+    'alignment-baseline', 'baseline-shift', 'class', 'clip', 'clip-path', 'clip-rule',
+    'color', 'color-interpolation', 'color-interpolation-filters', 'color-profile',
+    'color-rendering', 'cursor', 'd', 'direction', 'display', 'dominant-baseline',
+    'enable-background', 'externalResourcesRequired', 'fill', 'fill-opacity', 'fill-rule',
+    'filter', 'flood-color', 'flood-opacity', 'font-family', 'font-size', 'font-size-adjust',
+    'font-stretch', 'font-style', 'font-variant', 'font-weight', 'glyph-orientation-horizontal',
+    'glyph-orientation-vertical', 'image-rendering', 'kerning', 'letter-spacing',
+    'lighting-color', 'marker-end', 'marker-mid', 'marker-start', 'mask', 'opacity',
+    'overflow', 'pathLength', 'pointer-events', 'requiredFeatures', 'requiredExtensions',
+    'shape-rendering', 'stop-color', 'stop-opacity', 'stroke', 'stroke-dasharray',
+    'stroke-dashoffset', 'stroke-linecap', 'stroke-linejoin', 'stroke-miterlimit',
+    'stroke-opacity', 'stroke-width', 'style', 'text-anchor', 'text-decoration',
+    'text-rendering', 'transform', 'unicode-bidi', 'visibility', 'word-spacing',
+    'writing-mode'
+  ]),
+  function() { return true; }
+);
 
-var missedTags;
-var missedAttrs;
 var parsingStatus;
 
 /**
- * Main function
+ * Execute transform operation on the given path and return result path
  * 
  */
-function parseSvg(svgData) {
+function transformPath(path, transform) {
+  return (new SvgPath(path)).transform(transform).toString();
+}
 
-  missedTags = [];
-  missedAttrs = [];
+
+/**
+ * Parse SVG node and return result path
+ * 
+ */
+function parseNode(node, ignored) {
+  var transform = null;
+  var path = null;
+
+  // Attributes parsing
+  if (node.attributes) {
+    var attrLen = node.attributes.length;
+    for (var i = 0; i < attrLen; i++) {
+      var attr = node.attributes[i];
+      if (allowedAttrs[ attr.name ] !== true ){
+        continue;
+      }
+      switch (attr.name) {
+      case 'transform':
+        transform = attr.value;
+        break;
+      case 'd':
+        path = attr.value;
+        break;
+      default:
+        if (ignored.attrs.indexOf(attr.name) < 0){
+          ignored.attrs.push(attr.name);
+        }
+      }
+    }
+  }
+
+  // Tags parsing
+  switch (node.tagName) {
+  case 'g':
+    if (node.childNodes.length > 0){
+      path = parseNodeList(node.childNodes, ignored);
+    } else {
+      path = null;
+    }
+    break;
+  case 'path':
+    break;
+  default:
+    if (ignored.tags.indexOf(node.tagName) < 0){
+      ignored.tags.push(node.tagName);
+    }
+  }
+
+  if (path && transform) {
+    path = transformPath(path, transform);
+  }
+
+  return path;
+
+}
+
+/**
+ * Parse SVG document node list and return result path
+ * 
+ */
+function parseNodeList(nodeList, ignored) {
+  var path = [];
+  for (var i = 0; i < nodeList.length; i++) {
+    if ( allowedTags[ nodeList[i].tagName ] === true){
+      continue;
+    }
+    path[i] = parseNode(nodeList[i], ignored);
+  }
+  return path.join(' ');
+}
+
+/**
+ * Main function.
+ * Convert svg document with different tags to the compound path document
+ * 
+ * @param string svgData SVG document
+ * 
+ * @return {
+ *    path,
+ *    x, y, width, height,
+ *    ignoredTags[],
+ *    ignoredAttrs[],
+ *    ok,
+ * }
+ * 
+ */
+
+function parseSvg(svgData) {
+  var ignored = {
+    tags: [],
+    attrs:[],
+  };
+
   parsingStatus = false;
 
   var path = null;
@@ -90,12 +142,14 @@ function parseSvg(svgData) {
   var height = '100%';
 
   var doc = (new XMLDOMParser()).parseFromString(svgData, 'application/xml');
+  //var doc = (new XMLDOMParser()).parseFromString('asdfsdfas', 'application/xml');
   var svgTag = null;
 
-  if (typeof doc != 'undefined')
+  if (doc){
     svgTag = doc.getElementsByTagName('svg')[0];
+  }
 
-  if (typeof svgTag != 'undefined') {
+  if (svgTag) {
 
     var viewBox = _.map((svgTag.getAttribute('viewBox') || '').split(' '),
         function(val) {
@@ -113,112 +167,23 @@ function parseSvg(svgData) {
     width = viewBox[2] || attr.width || width;
     height = viewBox[3] || attr.height || height;
 
-    if (path = parseNodeList(svgTag.childNodes))
+    path = parseNodeList(svgTag.childNodes, ignored);
+    if (path){
       parsingStatus = true;
+    }
   }
 
-  return new Object({
+  return {
     'path' : path ? path : null,
     'x' : x,
     'y' : y,
     'width' : width,
     'height' : height,
-    'tags' : missedTags.length > 0 ? missedTags : null,
-    'attr' : missedAttrs.length > 0 ? missedAttrs : null,
+    'ignoredTags' : ignored.tags.length > 0 ? ignored.tags : null,
+    'ignoredAttributes' : ignored.attrs.length > 0 ? ignored.attrs : null,
     'ok' : parsingStatus,
-  });
+  };
 
-};
-
-/**
- * Combine several paths into the compound path
- * 
- */
-function pathMerge(paths) {
-  var result = null;
-  paths.forEach(function(val, index, array) {
-    if (val && typeof val == 'string') {
-      if (!result)
-        result = val;
-      else
-        result = result + ' ' + val;
-    }
-  });
-  return result;
-}
-
-/**
- * Parse SVG document node list and return result path
- * 
- */
-function parseNodeList(nodeList) {
-  var path = [];
-  for (var i = 0; nodeList && (i < nodeList.length); i++) {
-    if (typeof nodeList[i].tagName != 'string'
-        || allowedTags.indexOf(nodeList[i].tagName) < 0)
-      continue;
-    path[i] = parseNode(nodeList[i]);
-  }
-  return pathMerge(path);
-}
-
-/**
- * Parse SVG node and return result path
- * 
- */
-function parseNode(node) {
-  var transform = null;
-  var path = null;
-
-  // Attributes parsing
-  if (node.attributes) {
-    for (var i = 0; i < node.attributes.length; i++) {
-      var attr = node.attributes[i];
-      if (allowedAttrs.indexOf(attr.name) < 0)
-        continue;
-      switch (attr.name) {
-      case 'transform':
-        transform = attr.value;
-        break;
-      case 'd':
-        path = attr.value;
-        break;
-      default:
-        if (missedAttrs.indexOf(attr.name) < 0)
-          missedAttrs.push(attr.name);
-      }
-    }
-  }
-
-  // Tags parsing
-  switch (node.tagName) {
-  case 'g':
-    if (node.childNodes.length > 0)
-      path = parseNodeList(node.childNodes);
-    else
-      path = null;
-    break;
-  case 'path':
-    break;
-  default:
-    if (missedTags.indexOf(node.tagName) < 0)
-      missedTags.push(node.tagName);
-  }
-
-  if (path && transform) {
-    path = transformPath(path, transform);
-  }
-
-  return path;
-
-}
-
-/**
- * Execute transform operation on the given path and return result path
- * 
- */
-function transformPath(path, transform) {
-  return (new SvgPath(path)).transform(transform).toString();
 }
 
 module.exports = parseSvg;
